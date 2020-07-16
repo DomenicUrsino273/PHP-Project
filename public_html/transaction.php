c814d073fe76e13302c74b9cdd705d8a5d4b1df2
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-16 11:56:42 +1000

    Update transaction.php

--- public_html/transaction.php
+++ public_html/transaction.php
@@ -30,24 +30,21 @@
     }
 ?>
 
-<div class="wrapper">
-    <div class="response container">
-        <div class="content">
-            <div class="icon">
-            <img src="/images/<?php echo($icon)?>.svg" alt="">
-            </div>
-
-            <h1><?php echo($header)?></h1>
-            <section>
-                <p><?php echo($message)?></p>
-            </section>
-            <section>
-                <a class="button primary back" href="/index.php">
-                    <span>Test Another Transaction</span>
-                </a>
-            </section>
-        </div>
-    </div>
+<center><ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div></center> 
 </div>
 
 <aside class="drawer dark">
