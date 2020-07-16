d0315131973f506c4a43c69efa307fd529e400f4
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-16 11:41:06 +1000

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -19,16 +19,16 @@
 		
 		<div class="checkout container">
           <!-- Button trigger modal -->
-            <center><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            <p><center><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
              Click Me!
-            </button></center> 
+            </button></center></p>
 
              <!-- Modal -->
              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
              <div class="modal-dialog" role="document">
              <div class="modal-content">
              <div class="modal-header">
-             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+             <h5 class="modal-title" id="exampleModalLabel">Domenic's eCommerce Demo</h5>
 			 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
              <span aria-hidden="true">&times;</span>
              </button>
