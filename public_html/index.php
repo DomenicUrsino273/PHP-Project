5401716657feb27f59de315e2235f1955d6c9584
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-16 11:31:10 +1000

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -7,36 +7,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <body>
-    <div class="wrapper">
-        <div class="checkout container">
-
-            <header>
-                <h1>Hi, <br>Let's test a transaction</h1>
-                <p>
-                    Make a test payment with Braintree using PayPal or a card
-                </p>
-            </header>
-
-            <p><form method="post" id="payment-form" action="<?php echo $baseUrl;?>checkout.php">
-                <section>
-                    <label for="amount">
-                        <span class="input-label">Amount</span>
-                        <div class="input-wrapper amount-wrapper">
-                            <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                        </div>
-                    </label>
-
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin"></div>
-                    </div>
-                </section>
-
-                <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button class="btn btn-primary" type="submit"><span>Test Transaction</span></button>
-            </form></p>
-        </div>
-    </div>
-	
+ 	
 	<div class="wrapper">
         <div class="checkout container">
 
