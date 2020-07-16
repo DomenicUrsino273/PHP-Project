f22d1464ff97bfb4b5d67c88bde3fbe2db5304ff
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-16 13:55:05 +1000

    Update transaction.php

--- public_html/transaction.php
+++ public_html/transaction.php
@@ -42,7 +42,20 @@
     </div>
 </div>
 
-
+<form>
+<h2>Testing Form</h2>
+  <div class="form-group row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div></form>
 
 
 
