935e6b7f0e0fdfa759bd6eb02c42631e012b1b6d
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-19 14:54:05 +1000

    Updated Response Pages

--- public_html/transaction.php
+++ public_html/transaction.php
@@ -39,121 +39,67 @@
     </div>
 </div>
 
-  <article class="content compact">
-        <section>
-            <h5>Transaction</h5>
-            <table cellpadding="0" cellspacing="0">
-                <tbody>
-                    <tr>
-                        <td>id</td>
-                        <td><?php echo($transaction->id)?></td>
-                    </tr>
-                    <tr>
-                        <td>type</td>
-                        <td><?php echo($transaction->type)?></td>
-                    </tr>
-                    <tr>
-                        <td>amount</td>
-                        <td><?php echo($transaction->amount)?></td>
-                    </tr>
-                    <tr>
-                        <td>status</td>
-                        <td><?php echo($transaction->status)?></td>
-                    </tr>
-                    <tr>
-                        <td>created_at</td>
-                        <td><?php echo($transaction->createdAt->format('Y-m-d H:i:s'))?></td>
-                    </tr>
-                    <tr>
-                        <td>updated_at</td>
-                        <td><?php echo($transaction->updatedAt->format('Y-m-d H:i:s'))?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
 
-        <section>
-            <h5>Payment</h5>
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Transaction Details</button>
 
-            <table cellpadding="0" cellspacing="0">
-                <tbody>
-                    <tr>
-                        <td>token</td>
-                        <td><?php echo($transaction->creditCardDetails->token)?></td>
-                    </tr>
-                    <tr>
-                        <td>bin</td>
-                        <td><?php echo($transaction->creditCardDetails->bin)?></td>
-                    </tr>
-                    <tr>
-                        <td>last_4</td>
-                        <td><?php echo($transaction->creditCardDetails->last4)?></td>
-                    </tr>
-                    <tr>
-                        <td>card_type</td>
-                        <td><?php echo($transaction->creditCardDetails->cardType)?></td>
-                    </tr>
-                    <tr>
-                        <td>expiration_date</td>
-                        <td><?php echo($transaction->creditCardDetails->expirationDate)?></td>
-                    </tr>
-                    <tr>
-                        <td>cardholder_name</td>
-                        <td><?php echo($transaction->creditCardDetails->cardholderName)?></td>
-                    </tr>
-                    <tr>
-                        <td>customer_location</td>
-                        <td><?php echo($transaction->creditCardDetails->customerLocation)?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
-        <?php if (!is_null($transaction->customerDetails->id)) : ?>
-        <section>
-            <h5>Customer Details</h5>
-            <table cellpadding="0" cellspacing="0">
-                <tbody>
-                    <tr>
-                        <td>id</td>
-                        <td><?php echo($transaction->customerDetails->id)?></td>
-                    </tr>
-                    <tr>
-                        <td>first_name</td>
-                        <td><?php echo($transaction->customerDetails->firstName)?></td>
-                    </tr>
-                    <tr>
-                        <td>last_name</td>
-                        <td><?php echo($transaction->customerDetails->lastName)?></td>
-                    </tr>
-                    <tr>
-                        <td>email</td>
-                        <td><?php echo($transaction->customerDetails->email)?></td>
-                    </tr>
-                    <tr>
-                        <td>company</td>
-                        <td><?php echo($transaction->customerDetails->company)?></td>
-                    </tr>
-                    <tr>
-                        <td>website</td>
-                        <td><?php echo($transaction->customerDetails->website)?></td>
-                    </tr>
-                    <tr>
-                        <td>phone</td>
-                        <td><?php echo($transaction->customerDetails->phone)?></td>
-                    </tr>
-                    <tr>
-                        <td>fax</td>
-                        <td><?php echo($transaction->customerDetails->fax)?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>i
-        <?php endif; ?>
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Transaction Details</h4>
+      </div>
+      <div class="modal-body">
 
+      <table width="200" border="1">
+  <tbody>
+    <tr>
+        <td>id</td>
+        <td><?php echo($transaction->id)?></td>
+    </tr>
+    <tr>
+     <td>type</td>
+      <td><?php echo($transaction->type)?></td>
+    </tr>
+    <tr>
+      <td>amount</td>
+      <td><?php echo($transaction->amount)?></td>
+    </tr>
+    <tr>
+       <td>status</td>
+        <td><?php echo($transaction->status)?></td>
+    </tr>
+    <tr>
+       <td>created_at</td>
+       <td><?php echo($transaction->createdAt->format('Y-m-d H:i:s'))?></td>
+    </tr>
+    <tr>
+      <td>updated_at</td>
+      <td><?php echo($transaction->updatedAt->format('Y-m-d H:i:s'))?></td>
+    </tr>
+  </tbody>
+</table>
 
+            
+                  
+                        
+       </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
 
-
-
+  </div>
+</div>
+      
+      
+      
+      
+      
+      
 </body>
 </html>
