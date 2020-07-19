ab8a9350174ec297be73a87bd24118faedc2b2e3
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-19 14:44:47 +1000

    Update transaction.php

--- public_html/transaction.php
+++ public_html/transaction.php
@@ -5,6 +5,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <body>
+
 <?php
     require_once("../includes/header.php");
     if (isset($_GET["id"])) {
@@ -31,45 +32,13 @@
 <div class="wrapper"> 
     <div class="response container">
         <div class="content">
-
 	   <div class="alert alert-success" role="alert">
        <center><h2><?php echo($header)?></h2></center>
        </div>
-
         </div>
     </div>
 </div>
 
-<table width="200" border="1">
-  <tbody>
-    <tr>
-      <th scope="col">Transaction</th>
-    </tr>
-    <tr>
-     <td>id</td>
-     <td><?php echo($transaction->id)?></td>
-    </tr>
-    <tr>
-      <td>type</td>
-      <td><?php echo($transaction->type)?></td>
-    </tr>
-    <tr>
-      <td>amount</td>
-      <td><?php echo($transaction->amount)?></td>
-    </tr>
-    <tr>
-      <td>status</td>
-      <td><?php echo($transaction->status)?></td>
-    </tr>
-    <tr>
-     <td>created_at</td>
-      <td><?php echo($transaction->createdAt->format('Y-m-d H:i:s'))?></td>>
-    </tr>
-    <tr>
-     <td>updated_at</td>
-     <td><?php echo($transaction->updatedAt->format('Y-m-d H:i:s'))?></td>
-    </tr>
-  </tbody>
-</table>
+
 </body>
 </html>
