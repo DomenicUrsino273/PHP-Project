72858e483d3f2527811dbd39356f5bda6f3c766c
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-19 14:03:41 +1000

    Update checkout.php

--- public_html/checkout.php
+++ public_html/checkout.php
@@ -2,12 +2,10 @@
 require_once("../includes/braintree_init.php");
 
 $amount = $_POST["amount"];
-$firstName = $_POST["firstName"];
 $nonce = $_POST["payment_method_nonce"];
 
 $result = $gateway->transaction()->sale([
     'amount' => $amount,
-	'firstName' => $firstName,
     'paymentMethodNonce' => $nonce,
     'options' => [
         'submitForSettlement' => true
