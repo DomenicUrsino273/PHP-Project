0fbe8cf1c9c6a780a14984d3e072bd8b88affeff
DomenicUrsino273 <68359155+DomenicUrsino273@users.noreply.github.com>
2020-07-28 10:18:41 +1000

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,16 +6,6 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-<script type="text/javascript">
-   
-   function myFunction() {
-		   
-   var paypalAmount = document.getElementById("amount").value;
-   
-	   }
-</script>
-
 <body>
  	
 	<div class="wrapper">
@@ -57,7 +47,6 @@
                         <div class="input-wrapper amount-wrapper">
                             <input id="amount" name="amount" type="tel" min="1" placeholder="" value="100">
                         </div>
-						 <button class="btn btn-success" onclick="myFunction()" type="submit"><span>Load PayPal Amount</span></button>
                     </label>
 					
                     <p><div class="bt-drop-in-wrapper">
@@ -81,14 +70,13 @@
     <script>
         var form = document.querySelector('#payment-form');
         var client_token = "<?php echo($gateway->ClientToken()->generate()); ?>"; //Generates Client Auth
-
         braintree.dropin.create({
           authorization: client_token,
           selector: '#bt-dropin',
           paypal: {
-    flow: 'checkout',
-	amount: myFunction(),
-    currency: 'AUD'
+          flow: 'checkout',
+	      amount: Math.random(), 
+          currency: 'AUD'
   },
 		card: {       // Adding CardHolderName field to collect details
         cardholderName: {
